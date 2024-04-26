041c35710aa8f9555fa876537d9ea8046f1c2a6b
Cooper Pierce <cooper@semgrep.com>
2024-04-26 13:21:48 -0700

    feat: separate pre-commit repository
    Breaks out the pre-commit hook definition into a separate repo. Since
    [pre-commit](https://pre-commit.com/) clones the entire repo this should
    help with installation times. Additionally, it removes a dependency of
    the pre-commit hook on various submodules which are not related.
    
    This also changes the docker images to use semgrep/semgrep instead of
    returntocorp/semgrep.
    
    See also <https://github.com/semgrep/semgrep/issues/10126>.

---
+++ setup.py
@@ -0,0 +1,11 @@
+# type: ignore
+# Used for pre-commit since it expects a setup.py in repo root
+# for actual setup.py see cli/setup.py
+from setuptools import setup
+
+setup(
+    name="semgrep_pre_commit_package",
+    version="1.70.0",
+    install_requires=["semgrep==1.70.0"],
+    packages=[],
+)
