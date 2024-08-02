a17c837d8c2fc3df3f777acdb81fcddc4e813cba
Yosef Alsuhaibani <yosefalsuhaibani@Yosefs-MacBook-Pro-2.local>
2024-08-02 15:06:01 -0400

    take arg

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,4 +1,4 @@
-VERSION=1.82.0
+VERSION=$1
 sed -ie "s/\(version\)=\"[0-9.]*\"\,/\1=\"$VERSION\"\,/" ../setup.py
 sed -ie "s/\(semgrep\)==[0-9.]*/\1==$VERSION/" ../setup.py
 sed -ie "s/\(rev\:*\) \'v[0-9.]*\'/\1 \'v$VERSION\'/" ../README.md
