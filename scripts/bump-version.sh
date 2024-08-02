7de67c40bbea08b29f59421b54cdb05a173c100a
Yosef Alsuhaibani <yosefalsuhaibani@Yosefs-MacBook-Pro-2.local>
2024-08-02 15:20:36 -0400

    Attempt at script1

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,4 +1,13 @@
 VERSION=$1
-sed -ie "s/\(version\)=\"[0-9.]*\"\,/\1=\"$VERSION\"\,/" ../setup.py
-sed -ie "s/\(semgrep\)==[0-9.]*/\1==$VERSION/" ../setup.py
-sed -ie "s/\(rev\:*\) \'v[0-9.]*\'/\1 \'v$VERSION\'/" ../README.md
+
+# Do text substitution in setup.py & README.md
+sed -ie "s/\(version\)=\"[0-9.]*\"\,/\1=\"$VERSION\"\,/" setup.py
+sed -ie "s/\(semgrep\)==[0-9.]*/\1==$VERSION/" setup.py
+sed -ie "s/\(rev\:*\) \'v[0-9.]*\'/\1 \'v$VERSION\'/" README.md
+
+# commit & tag & push code
+git add setup.py README.md
+git commit -m "Bump to version $VERSION"
+
+git tag $VERSION $(git rev-parse HEAD)
+git push origin tag $VERSION
