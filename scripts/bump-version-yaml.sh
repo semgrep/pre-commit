76e46417bdb6fdac2c4be1ddf3004918b6a12945
Yosef Alsuhaibani <yosefalsuhaibani@Yosefs-MacBook-Pro-2.local>
2024-08-05 11:05:41 -0400

    Use right flag to checkout branch + change file name

--- scripts/bump-version-yaml.sh
+++ scripts/bump-version-yaml.sh
@@ -1,8 +1,9 @@
 VERSION=$1
+
 sed -ie "s/\(rev\:*\) \'v[0-9.]*\'/\1 \'v$VERSION\'/" .pre-commit-config.yaml
 
 # commit & tag & push code
-git checkout -c bump-pre-commit-yaml-to-$VERSION
+git checkout -b bump-pre-commit-yaml-to-$VERSION
 git add .pre-commit-config.yaml
 git commit -m "Bump to version $VERSION"
 
--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -6,7 +6,7 @@ sed -ie "s/\(semgrep\)==[0-9.]*/\1==$VERSION/" setup.py
 sed -ie "s/\(rev\:*\) \'v[0-9.]*\'/\1 \'v$VERSION\'/" README.md
 
 # commit & tag & push code
-git checkout -c bump-pre-commit-to-$VERSION
+git checkout -b bump-pre-commit-to-$VERSION
 git add setup.py README.md
 git commit -m "Bump to version $VERSION"
 
