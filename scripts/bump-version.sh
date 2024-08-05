1a3e71df793504341123619e9fa778815b8a5dbf
Yosef Alsuhaibani <yosefalsuhaibani@Yosefs-MacBook-Pro-2.local>
2024-08-05 12:07:16 -0400

    Commit via workflow not via script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,11 +4,3 @@ VERSION=$1
 sed -ie "s/\(version\)=\"[0-9.]*\"\,/\1=\"$VERSION\"\,/" setup.py
 sed -ie "s/\(semgrep\)==[0-9.]*/\1==$VERSION/" setup.py
 sed -ie "s/\(rev\:*\) \'v[0-9.]*\'/\1 \'v$VERSION\'/" README.md
-
-# commit & tag & push code
-git checkout -b bump-pre-commit-to-$VERSION
-git add setup.py README.md
-git commit -m "Bump to version $VERSION"
-
-git tag $VERSION $(git rev-parse HEAD)
-git push origin tag $VERSION
