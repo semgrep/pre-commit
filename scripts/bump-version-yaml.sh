dc756c01af33f204521e57c93acf10067734b3ac
Yosef Alsuhaibani <yosefalsuhaibani@Yosefs-MacBook-Pro-2.local>
2024-08-06 16:32:15 -0400

    Don't commit

--- scripts/bump-version-yaml.sh
+++ scripts/bump-version-yaml.sh
@@ -1,10 +1,3 @@
 VERSION=$1
 
 sed -ie "s/\(rev\:*\) \'v[0-9.]*\'/\1 \'v$VERSION\'/" .pre-commit-config.yaml
-
-# commit & tag & push code
-git checkout -b bump-pre-commit-yaml-to-$VERSION
-git add .pre-commit-config.yaml
-git commit -m "Bump to version $VERSION"
-
-git push origin HEAD
