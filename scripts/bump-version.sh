72cc2d1201fa176d6b3a75b350ac927b2335fc5a
Yosef Alsuhaibani <yosefalsuhaibani@Yosefs-MacBook-Pro-2.local>
2024-08-12 14:12:30 -0400

    Add work from other fork

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,6 +1,9 @@
 VERSION=$1
 
 # Do text substitution in setup.py & README.md
-sed -ie "s/\(version\)=\"[0-9.]*\"\,/\1=\"$VERSION\"\,/" setup.py
-sed -ie "s/\(semgrep\)==[0-9.]*/\1==$VERSION/" setup.py
-sed -ie "s/\(rev\:*\) \'v[0-9.]*\'/\1 \'v$VERSION\'/" README.md
+sed "s/\(version\)=\"[0-9.]*\"\,/\1=\"$VERSION\"\,/" setup.py > tmp
+mv tmp setup.py
+sed "s/\(semgrep\)==[0-9.]*/\1==$VERSION/" setup.py > tmp
+mv tmp setup.py
+sed "s/\(rev\:\)\ 'v[0-9.]*'/\1\ \'v$VERSION\'/" README.md > tmp
+mv tmp README.md
