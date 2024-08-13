2ba84a454fa0a620d034b7d04cd3f1e225d4d315
Yosef Alsuhaibani <yosefalsuhaibani@Yosefs-MacBook-Pro-2.local>
2024-08-13 16:04:53 -0400

    Cooper edit

--- scripts/bump-version-yaml.sh
+++
@@ -1,3 +0,0 @@
-VERSION=$1
-sed "s/\(rev\:\)\ 'v[0-9.]*'/\1\ \'v$VERSION\'/" .pre-commit-config.yaml > tmp
-mv tmp .pre-commit-config.yaml
--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,9 +1,9 @@
 VERSION=$1
-
+OLD_VERSION=$(grep -o 'version=\"[0-9.]*\"' setup.py | sed "s/version=\"\([0-9.]*\)\"/\1/")
 # Do text substitution in setup.py & README.md
-sed "s/\(version\)=\"[0-9.]*\"\,/\1=\"$VERSION\"\,/" setup.py > tmp
+sed "s/$OLD_VERSION/$VERSION/" setup.py > tmp
 mv tmp setup.py
-sed "s/\(semgrep\)==[0-9.]*/\1==$VERSION/" setup.py > tmp
-mv tmp setup.py
-sed "s/\(rev\:\)\ 'v[0-9.]*'/\1\ \'v$VERSION\'/" README.md > tmp
+sed "s/$OLD_VERSION/$VERSION/" README.md > tmp
 mv tmp README.md
+sed "s/$OLD_VERSION/$VERSION/" .pre-commit-config.yaml > tmp
+mv tmp .pre-commit-config.yaml
