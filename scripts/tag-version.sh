66403682206f145189490aaa3e3dee0f0012b786
Yosef Alsuhaibani <72322110+yosefAlsuhaibani@users.noreply.github.com>
2024-08-14 17:42:11 -0400

    Update scripts/tag-version.sh
    Co-authored-by: Cooper Pierce <cooper@semgrep.com>

--- scripts/tag-version.sh
+++ scripts/tag-version.sh
@@ -1,4 +1,4 @@
-CURR_VERSION=$(grep -o 'version=\"[0-9.]*\"' setup.py | sed "s/version=\"\([0-9.]*\)\"/\1/")
+CURRENT_VERSION=$(grep -o 'version=\"[0-9.]*\"' setup.py | sed "s/version=\"\([0-9.]*\)\"/\1/")
 
-git tag "v${CURR_VERSION}" HEAD
-git push origin tag "v${CURR_VERSION}"
+git tag "v${CURRENT_VERSION}" HEAD
+git push origin tag "v${CURRENT_VERSION}"
