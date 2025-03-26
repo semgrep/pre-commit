79dd1d16fbc3ef583cc516710d419ea71ee10f45
Puneeth Chaganti <punchagan@muse-amuse.in>
2025-03-26 11:29:22 +0530

    Declare entry points to fix installation on Widnows

--- setup.py
+++ setup.py
@@ -8,4 +8,11 @@ setup(
     version="1.114.0",
     install_requires=["semgrep==1.114.0"],
     packages=[],
+    # To be able to use this package on Windows, we need to declare
+    # entry_points. See https://stackoverflow.com/a/73140567
+    entry_points={
+        "console_scripts": [
+            "semgrep = semgrep.console_scripts.entrypoint:main",
+        ]
+    },
 )
